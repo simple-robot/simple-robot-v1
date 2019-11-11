62934ba5dd93ada51f1bd1ede28ab974f5da490e
ForteScarlet <ForteScatlet@163.com>
2019-11-11 17:07:45 +0800

    debug, 更新update日志, 部署为1.3.11版本

--- src/main/java/com/forte/qqrobot/depend/AdditionalDepends.java
+++ src/main/java/com/forte/qqrobot/depend/AdditionalDepends.java
@@ -72,10 +72,12 @@ public class AdditionalDepends implements DependGetter {
         nameObjMap.forEach((k, v) -> {
             //只有当不是基础类型的时候才根据类型添加
             if(!BasicResourceWarehouse.isBasicType(v)){
-//                typeMap.put(v.getClass(), v);
-                typeMap.merge(v.getClass(), v, typeMapMergeThrows(v.getClass()));
+                // 类型重复，覆盖处理
+                typeMap.put(v.getClass(), v);
+//                typeMap.merge(v.getClass(), v, typeMapMergeThrows(v.getClass()));
             }
 //            nameMap.put(k, v);
+            // 名称绝对不可重复
             nameMap.merge(k, v, nameMapMergeThrows(k));
         });
         //构建
