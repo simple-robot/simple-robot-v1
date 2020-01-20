7dea25a29eb1b25dade83f013f2e126de982300b
ForteScarlet <ForteScatlet@163.com>
2020-01-20 15:47:19 +0800

    增加lang相关类，为语言转化做铺垫

---
+++ src/main/java/com/forte/lang/Language.java
@@ -0,0 +1,12 @@
+package com.forte.lang;
+
+/**
+ *
+ * 语言类，用于读取lang文件
+ * lang文件本质上也是一种properties文件
+ *
+ * @author ForteScarlet <[email]ForteScarlet@163.com>
+ * @since JDK1.8
+ **/
+public class Language {
+}
