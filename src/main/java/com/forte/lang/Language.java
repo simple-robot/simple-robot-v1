9f67cdc3818ec4f00ceaec956352ecd646f225d5
ForteScarlet <ForteScarlet@163.com>
2020-01-28 11:14:30 +0800

    language更新

--- src/main/java/com/forte/lang/Language.java
+++ src/main/java/com/forte/lang/Language.java
@@ -1,5 +1,9 @@
 package com.forte.lang;
 
+import sun.util.locale.BaseLocale;
+
+import java.util.Locale;
+
 /**
  *
  * 语言类，用于读取lang文件
@@ -9,4 +13,41 @@ package com.forte.lang;
  * @since JDK1.8
  **/
 public class Language {
+
+    private static final Locale DEFAULT_LOCALE = Locale.US;
+
+    /*
+        sun.desktop	=	windows
+        os.name	=	Windows 10
+        os.version	=	10.0
+        sun.cpu.isalist	=	amd64
+        os.arch	=	amd64
+
+        user.language	=	zh
+        user.country	=	CN
+
+
+        file.separator	=	\
+
+        file.encoding	=	UTF-8
+        sun.jnu.encoding	=	GBK
+
+     */
+    public static void main(String[] args) {
+//        System.getProperties().forEach((k, v) -> System.out.println(k + "\t=\t" + v));
+
+        String language = System.getProperties().getProperty("user.language");
+        String country = System.getProperties().getProperty("user.country");
+
+
+        String languageTag = language + "_" + country;
+
+        Locale def = Locale.getDefault();
+        System.out.println(def);
+
+        System.out.println(def.toString());
+
+        System.out.println(Locale.US.getDisplayName());
+
+    }
 }
---
+++ src/main/java/com/forte/qqrobot/CoreSystem.java
@@ -0,0 +1,12 @@
+package com.forte.qqrobot;
+
+
+/**
+ * 核心中类似于System的类
+ * @author ForteScarlet
+ */
+public class CoreSystem {
+
+
+
+}
