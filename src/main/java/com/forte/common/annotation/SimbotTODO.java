020acea68afe564a92eb26fa2a90fc6f3ebf17bb
laihang <iceberglh@163.com>
2020-08-19 13:54:52 +0800

    添加TODO备忘

--- src/main/java/com/forte/common/annotation/SimbotTODO.java
+++ src/main/java/com/forte/common/annotation/SimbotTODO.java
@@ -11,14 +11,16 @@ import static java.lang.annotation.ElementType.CONSTRUCTOR;
 /**
  *
  * 给开发者我自己看的。
- * 标注一些可能需要大改的地方。
+ * 标注一些可能需要大改的地方.
+ * 简单来说就是开发者的备忘录.
  *
+ * 非运行时，不会被加载
  * Created by lcy on 2020/8/19.
  *
  * @author lcy
  */
 @Documented
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 @Target({FIELD, TYPE, METHOD, PARAMETER, PACKAGE, CONSTRUCTOR})
 public @interface SimbotTODO {
 	String[] value() default {};
